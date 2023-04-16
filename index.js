0322434668cba488535a18efee4a24a49c880875
srikoti <ayushsinghsrikoti@gmail.com>
2023-04-16 23:05:59 +0530

    setting up partials and layouts

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 //use express router
 app.use('/' , require('./routes/index'));
+
+//set up view engine 
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
 
 app.listen(port , function(err){
     if(err){
