ac27fd49a3055f9d566f5cb5cc69aa1a26d8b87d
srikoti <ayushsinghsrikoti@gmail.com>
2023-04-17 23:15:23 +0530

    created sign in , sign up and mongo db added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,47 @@
+const user = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
     })
+}
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up' ,{
+        title: "bruhgram | sign up"
+    })
+}
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in' ,{
+        title: "bruhgram | sign in"
+    })
+}
+
+//get sign up , sign in data
+
+module.exports.create = function(req, res) {
+    if (req.body.password != req.body.confirm_password) {
+      return res.redirect('back');
+    }
+  
+    user.findOne({ email: req.body.email })
+      .then(existingUser => {
+        if (existingUser) {
+          return res.redirect('back');
+        }
+        return user.create(req.body)
+          .then(newUser => {
+            return res.redirect('/users/sign-in');
+          });
+      })
+      .catch(err => {
+        console.log('Error in finding or creating user: ', err);
+        return res.redirect('back');
+      });
+  }
+  
+
+module.exports.createSession = function(req,res){
+    
 }
