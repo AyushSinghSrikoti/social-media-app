eb697fe6e0e11be96aa1b7730c1a33be07c100e7
srikoti <ayushsinghsrikoti@gmail.com>
2023-05-26 21:27:25 +0530

    jwt and api

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,57 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res) {
+  try {
+    const posts = await Post.find({})
+      .sort('-createdAt')
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: {
+          path: 'user'
+        }
+      });
+
+    return res.json(200, {
+      message: "List of posts",
+      posts: posts // Use `posts` instead of `post`
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+};
+
+module.exports.destroy = async function(req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found"
+      });
+    }
+
+    // if (post.user == req.user.id){
+    await post.remove();
+
+    await Comment.deleteMany({ post: req.params.id });
+
+    return res.json(200, {
+      message: "Post and associated comments deleted successfully!"
+    });
+    // }else{
+    //     req.flash('error', 'You cannot delete this post!');
+    //     return res.redirect('back');
+    // }
+
+  } catch (err) {
+    console.log('********', err);
+    return res.status(500).json({
+      message: "Internal Server Error"
+    });
+  }
+};
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo');
 const dbUrl = 'mongodb://127.0.0.1:27017/bruhgram_development';
 const flash = require('connect-flash');
