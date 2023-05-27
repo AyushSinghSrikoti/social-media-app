f1617cb38557e1570662289ae662a3086bf2c3af
srikoti <ayushsinghsrikoti@gmail.com>
2023-05-27 23:46:00 +0530

    google oauth 2

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -35,7 +35,7 @@ module.exports.destroy = async function(req, res) {
       });
     }
 
-    // if (post.user == req.user.id){
+    if (post.user == req.user.id){
     await post.remove();
 
     await Comment.deleteMany({ post: req.params.id });
@@ -43,10 +43,11 @@ module.exports.destroy = async function(req, res) {
     return res.json(200, {
       message: "Post and associated comments deleted successfully!"
     });
-    // }else{
-    //     req.flash('error', 'You cannot delete this post!');
-    //     return res.redirect('back');
-    // }
+    }else{
+        return res.json(401,{
+          message:"cant delete post"
+        });
+    }
 
   } catch (err) {
     console.log('********', err);
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo');
 const dbUrl = 'mongodb://127.0.0.1:27017/bruhgram_development';
 const flash = require('connect-flash');
